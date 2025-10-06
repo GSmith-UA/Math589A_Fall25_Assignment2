db08467cf9765de45b16bceeb499f2695d970c44
Griffin Smith <griffinlsmith@arizona.edu>
2025-10-06 09:01:45 -0700

    empty

--- general_linear_solver.py
+++ general_linear_solver.py
@@ -81,6 +81,10 @@ def solve(A, b, TOL=1e-15):
 
         y = forwardSubstitution(L,b_perm[:r])
         if np.any(np.abs(b_perm[r:]) > TOL):
+            print(A)
+            print(b)
+            print(L)
+            print(U)
             print(b_perm[r:])
             print("Griffin's print bc I cannot see unit tests...")
             raise ValueError("inconsistent system: A x = b has no solution")
