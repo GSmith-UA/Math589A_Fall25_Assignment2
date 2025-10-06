49b3ab15a02ee6d9ee150c83427f976ebf425c9e
Griffin Smith <griffinlsmith@arizona.edu>
2025-10-06 09:21:20 -0700

    empty

--- plu_decomposition.py
+++ plu_decomposition.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def paqlu_decomposition_in_place(A,TOL=1e-12):
+def paqlu_decomposition_in_place(A,TOL=1e-15):
     m = A.shape[0] # Grab the number of rows...
     n = A.shape[1] # Grabs the number of columns
 
@@ -22,6 +22,10 @@ def paqlu_decomposition_in_place(A,TOL=1e-12):
         pivot_row = k + (idx//U[k:,k:].shape[1])
         pivot_col = k + (idx%U[k:,k:].shape[1])
 
+        # If the diagonal element is now close to zero... call us rank deficient and exit
+        if np.abs(U[pivot_row,pivot_col]) < TOL:
+            rank = k
+            break
         # Swap rows k and r in U
         U[[pivot_row,k],:] = U[[k,pivot_row],:]
         # Swap rows k and r in P
@@ -32,11 +36,6 @@ def paqlu_decomposition_in_place(A,TOL=1e-12):
         # Swap rows k and r in L (but only columns 1:k-1) or in python 0:k-1
         if k-1>=0:
             L[[pivot_row,k],0:k] = L[[k,pivot_row],0:k]
-
-        # If the diagonal element is now close to zero... call us rank deficient and exit
-        if np.abs(U[k,k]) < TOL:
-            rank = k
-            break
 
         for i in range(k+1,m):
             L[i,k] = U[i,k]/U[k,k]
