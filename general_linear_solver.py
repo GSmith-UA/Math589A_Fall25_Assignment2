4eab35bd863c0e09c57b90f1ed568591f548b57d
Griffin Smith <griffinlsmith@arizona.edu>
2025-10-06 17:47:42 -0700

    more prints

--- general_linear_solver.py
+++ general_linear_solver.py
@@ -87,6 +87,7 @@ def solve(A, b, TOL=1e-6):
                 print(b_perm[r:])
                 print(U)
                 print(L)
+                print(P.T@L@U@Q.T)
                 # print("Griffin's print bc I cannot see unit tests...")
                 raise ValueError("inconsistent system: A x = b has no solution")
         c1= backSubstitution(U,y)
