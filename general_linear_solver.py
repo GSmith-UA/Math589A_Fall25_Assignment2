85a545f82b0c35423f89efef4aadde9bb39d21c6
Griffin Smith <griffinlsmith@arizona.edu>
2025-10-06 21:22:48 -0700

    I guess we can change the requirements/specs whenever...

--- general_linear_solver.py
+++ general_linear_solver.py
@@ -81,15 +81,16 @@ def solve(A, b, TOL=1e-6):
         y = forwardSubstitution(L,b_perm[:r])
         if r < m:
             if np.any(np.abs(b_perm[r:]) > TOL):
-                print(A)
-                print(b)
-                print(r)
-                print(b_perm[r:])
-                print(U)
-                print(L)
-                print(P.T@L@U@Q.T)
+                # print(A)
+                # print(b)
+                # print(r)
+                # print(b_perm[r:])
+                # print(U)
+                # print(L)
+                # print(P.T@L@U@Q.T)
                 # print("Griffin's print bc I cannot see unit tests...")
-                raise ValueError("inconsistent system: A x = b has no solution")
+                c = None
+                #raise ValueError("inconsistent system: A x = b has no solution")
         c1= backSubstitution(U,y)
 
         c1= c1# Re order the columns
